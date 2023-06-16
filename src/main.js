1cbeee3999b35d9e9dc9713f9c6c9849edd1e27b
fuwb <962920136@qq.com>
2023-06-16 22:07:33 +0800

    add EventBus & vuex persistedState

--- src/main.js
+++ src/main.js
@@ -14,5 +14,9 @@ Vue.use(ElementUI)
 new Vue({
   router,
   store,
+  beforeCreate() {
+    //安装全局事件总线，$bus就是当前应用的vm
+    Vue.prototype.$bus = this
+  },
   render: h => h(App)
 }).$mount('#app')
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,24 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import user from "@/store/modules/user";
+import user from "@/store/modules/user"
+import persistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
     user
-  }
+  },
+  //引用 vuex-persistedState 对 state 数据持久化
+  plugins: [
+    // 只要调用 vuex 时，才会进行持久化操作。
+    persistedState({
+      // 存储方式
+      storage: window.sessionStorage,
+      // 存储 key
+      key: 'vuex_test',
+      // 存储模块
+      paths: ["user"]
+    })
+  ]
 })
